b9667aa648b3d372ee0cc36e929301dba29141e4
RiaPant <riapant200777@gmail.com>
2017-01-09 22:16:24 +0530

    Changed the HTML for the site.

--- lock/views.py
+++ lock/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def search(request):
+    return render(request, 'lock/search.html', {})
+
+def results(request):
+    return render(request, 'lock/results.html', {})
