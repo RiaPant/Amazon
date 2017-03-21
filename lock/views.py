87c53acb39e5db04d0c49170f90b23683754e8a6
RiaPant <riapant200777@gmail.com>
2017-03-21 18:03:17 +0530

    empty list updated

--- lock/views.py
+++ lock/views.py
@@ -10,8 +10,8 @@ def results(request):
 	form = SearchForm()
 	quantity=request.GET.get("quantity")	
 	query = request.GET.get("search")
-	qlist = Prime.objects.all().select_related('locker').values('locker__locker_name','locker__city', 'locker__state','locker__pincode','day2').distinct().order_by('locker__locker_name')
-	qlist1 = Standard.objects.all().select_related('locker').values('locker__locker_name','locker__city', 'locker__state','locker__pincode','day5').distinct().order_by('locker__locker_name')
+	qlist = Prime.objects.all().select_related('locker').values('locker__locker_name','locker__city','locker__locker_id', 'locker__state','locker__pincode','day2').distinct().order_by('locker__locker_name')
+	qlist1 = Standard.objects.all().select_related('locker').values('locker__locker_name','locker__city','locker__locker_id', 'locker__state','locker__pincode','day5').distinct().order_by('locker__locker_name')
 	if query:
 		qlist = qlist.filter(Q(locker__city=query)|Q(locker__state__icontains=query)|Q(locker__pincode__icontains=query)).order_by('locker__locker_name')
 		qlist1 = qlist1.filter(Q(locker__city=query)|Q(locker__state__icontains=query)|Q(locker__pincode__icontains=query)).order_by('locker__locker_name')
@@ -22,8 +22,23 @@ def item(request):
     form = ItemForm()
     return render(request,'lock/items.html', {'form': form})
 
-def success(request):
-	return render(request, 'lock/success.html',{})
+def success(request,quantity="1", name="1"):
+	q1 = request.GET.get("chooseone")
+	quantity=int(quantity)
+	if q1=="Prime":
+		obj=Prime.objects.get(locker=name)
+		if obj.day2>=quantity:
+			obj.day2=obj.day2-quantity
+			obj.save()
+	elif q1=="Standard":
+		obj=Standard.objects.get(locker=name).distinct()
+		if obj.day5>=quantity:
+			obj.day5=obj.day5-quantity
+			obj.save()
+	obj=Table1.objects.get(locker_id=name)
+	address = obj.city+", "+ obj.state + "--" + obj.pincode
+	name = obj.locker_name	
+	return render(request, 'lock/success.html',{'quantity':quantity,'name': name,'address':address})
 '''	q1 = request.GET.get("chooseone")
 	q3 = request.GET.get('lockerName')	
 	if q1 == 'yes':
